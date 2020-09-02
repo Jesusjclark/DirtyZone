895dd07126997d3ed8c725089fe9a5516d256dd2
jesusjclark <jesusjclark@gmail.com>
2020-09-01 20:05:48 -0400

    carrusel

--- resources/views/distribuidorbarquisimeto.blade.php
+++ resources/views/distribuidorbarquisimeto.blade.php
@@ -9,7 +9,11 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-        <script type="text/javascript" >
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+          <script type="text/javascript" >
+
           $(document).ready(function() {
               $('#tableData').DataTable();
               $('#tableData2').DataTable();
@@ -25,7 +29,7 @@
             }
             .responsive {
             width: 100%;
-            max-width: 400px;
+            max-width: 800px;
             height: auto;
           }
           .titulo{
@@ -47,28 +51,56 @@
                 <div class="title m-b-md">    
                   <a href="/">
 
-                    <img src="/img/loguito.jpeg"style="
-                    width: 100px;
-                    height: 100px;"></a><p class="titulo">Distribuidor<br>Barquisimeto</p>
-                  
+                    <img src="/img/loguito.png"class="responsive"></a><p class="titulo">
                 </div>
-                <br>
-                <br>
-                 <img src="/img/promo.png" class="responsive">
-                  >
-                  <br>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
 
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+
+              <div class="item active">
+                <img src="/img/promo.png" alt="Los Angeles" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3>Pollo</h3>
+                  <p>El mas fresco</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="/img/promo2.png" alt="Los Angeles" style="width:100%;">
+                <div class="carousel-caption">
+                  <h3>Mortadela</h3>
+                  <p>La mejor del mercado</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Anterior</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Siguiente</span>
+            </a>
+          </div>
                
                 <box>
                   <body> <br>
-                    <h2 style="font-family: 'Nunito', sans-serif, bold; height:80px;align-content: center;font-size: 20px; ">Sigue nuestras redes: </h2>
+                    <h2 style="font-family: 'Nunito', sans-serif, bold; height:80px;align-content: center;font-size: 30px; ">Sigue nuestras redes: </h2>
                     <div class="links">
-                       <a href="https://twitter.com/MayoristaBqto_" style="padding-left: 20px"> 
+                       <a href="https://twitter.com/MayoristaBqto_" style=""> 
                         <img src="/img/twitter.png" alt="Avatar" class="avatar" style=" padding: 20px;">
                        </a>
                         <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwa.me%2Fmessage%2FAZDGKXD6XOOVA1%3Ffbclid%3DIwAR0PAjK3cNxkT5ReeyJB_MwrWYOsrW9NxJo4sAopbrEeV-7yMft5pGk-K0o&h=AT30rP5CeixDPKi4ZrND_RN_XvJEJaoJjA6jvoq65ExWsmWOlwO3Q6baZVkj1IE0d4-AWO3geBSEvIThDkWeLzxPRQSSbXA_9PEn0AecJ9oBx-pVdNb2u22Q7crw&__tn__=-UK*F&c[0]=AT12uk5_r0B7zmPIrho84q5UOpNs8vlaWYm3cz-_yt4wCSx3WOhX3I1Ju6lbt6xSD3G8JIb6nIBfkSrRyIVgotoHPPLrgWgDU0CPVtta-Zng1EqI-UcS_5UfLqY0JDIOPVJf7UXFNNsmIhSi8IK8ZewcjvwhT7Z_RvfTxsJZOlDBr86dL6WoMBbh7Q" style="padding-left: 10px">
                             <img src="/img/whatsapp.png" alt="Avatar" class="avatar" style=" padding: 20px;">
-                       </a>
+                       </a><
                         <a href="http://facebook.com/DistribuidorBarquisimeto" style="padding-left: 20px"> 
                           <img src="/img/facebook.png" alt="Avatar" class="avatar" style=" padding: 20px;">
                         </a>
